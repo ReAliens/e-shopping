5331312b3dcc203d4c8908e700ab6c67ae85796f
Ahmed <ahmedreda152@gmail.com>
2022-07-16 12:48:04 +0200

    fix conflict

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Header from './pages/Home/Header';
+import Header from './components/Header';
 import AppRoutes from './routes';
 
 class App extends Component {
